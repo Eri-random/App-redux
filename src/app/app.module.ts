d16b830e4040a7de1280dec3f5089e16611db24b
Eri-random <brenherrera@alumno.unlam.edu.ar>
2024-02-04 16:07:36 -0300

    division de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoModule } from './todos/todo.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    TodoModule,
     AppRoutingModule
   ],
   providers: [],
