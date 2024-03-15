28b15560a4392c01c4a0a087d90badeabd6a9d71
Erika <brenherrera@alumno.unlam.edu.ar>
2024-03-15 15:07:25 -0300

    agrego edicion de todo

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -33,6 +33,9 @@ export class TodoItemComponent implements OnInit {
   }
 
   editar(){
+
+    this.txtInput.setValue(this.txtInput.value);
+    
     this.editando = true;
     setTimeout(() =>{
       this.txtInputFisico.nativeElement.select()
@@ -40,7 +43,17 @@ export class TodoItemComponent implements OnInit {
   }
 
   terminarEdicion(){
+
     this.editando=false;
+
+    if(this.txtInput.invalid) return;
+    if(this.txtInput.value === this.todo.texto) return;
+
+    this.store.dispatch(actions.editar({
+      id:this.todo.id,
+      texto:this.txtInput.value
+    })
+    )
   }
 
 
